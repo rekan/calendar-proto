a3257501bbbfce826d275d2022d69d312ef5eb48
rekan <rekanagy06@gmail.com>
2017-05-25 14:00:10 +0200

    initial commit

---
+++ src/main/java/calendarproto/CalendarProtoApplication.java
@@ -0,0 +1,12 @@
+package calendarproto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarProtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarProtoApplication.class, args);
+	}
+}
