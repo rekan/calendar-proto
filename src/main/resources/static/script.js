f0960cba21ea7ced51d83371b63aa57c539cb184
rekan <rekanagy06@gmail.com>
2017-06-06 11:58:29 +0200

    selectable days and data shown in alerts

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -2,6 +2,19 @@ $(function() {
 
 
     $('#calendar').fullCalendar({
+        firstDay: 1,
+        header: {
+            left:   'title',
+            center: 'month,agendaWeek,agendaDay',
+            right:  'today prev,next'
+        },
+        selectable: true,
+        select: function( start, end, jsEvent, view ) {
+            alert(start);
+            alert(end);
+            alert(jsEvent.pageX + ',' + jsEvent.pageY);
+            alert(view.name)
+        }
 
     })
 
